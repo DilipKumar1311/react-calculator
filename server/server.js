fbb3667abcae9c6c93b86cfa4cf6d424681ff4c2
Dilip <diliptrainee@gmail.com>
2017-08-18 16:45:16 +0530

    Modification related to CSS

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ function requestHandler(request, response) {
     url.parse(request.url).pathname // path to resource requested by client
   );
 
-  // use the exists method of the fs module to check if the requestedResource
+  // use the exists method of the fs module to check if the requestedResource 
   // exists.
   fs.exists(requestedResource, function(exists) {
     // check if file does't exist and return a 404 status (File not found)
@@ -31,24 +31,24 @@ function requestHandler(request, response) {
     if (fs.statSync(requestedResource).isDirectory()) {
       requestedResource += '/index.html';
     }
-
-    // Finally, we read the requested file (asynchronously) and send it's
+    
+    // Finally, we read the requested file (asynchronously) and send it's 
     // content to the client
     fs.readFile(
       requestedResource, // path to requested resource
       "binary", // read the requested resource as a binary file
       function(err, file) { // call back function to handle end of file reading
 
-      // If an error occured while reading the file, send the error message
+      // If an error occured while reading the file, send the error message 
       // with a status code of 500 (Internal server error)
-      if (err) {
+      if (err) {        
         response.writeHead(500, {"Content-Type": "text/plain"});
         response.write(err + "\n");
         response.end();
         return;
       }
 
-      // Helper object to map requested content types (extension) to response
+      // Helper object to map requested content types (extension) to response 
       // mime types
       const contentTypesByExtension = {
         '.html': "text/html",
@@ -63,7 +63,7 @@ function requestHandler(request, response) {
         path.extname(requestedResource)
       ];
 
-      // if the requested resource maps to any of our content type extension,
+      // if the requested resource maps to any of our content type extension, 
       // then set the Content-Type field for our response headers.
       if (contentType) {
         headers["Content-Type"] = contentType;
